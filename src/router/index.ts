1146eadf2c83504ed6fcde3e6f8214776ea86946
露西 <lucy@luxideMacBook-Air.local>
2023-01-04 14:32:40 +0800

    fixed router scroll

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,16 @@ import routes from './routes'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  scrollBehavior: (to, from, savePosition) => {
+    console.log(savePosition)
+    if (savePosition) {
+      return savePosition
+    } else {
+      return {
+        top: 0,
+      }
+    }
+  },
 })
 
 export default router
