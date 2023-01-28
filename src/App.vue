3e0ee637bf00728164fe9ded85fc13304264dbd5
露西 <lucy@luxideMacBook-Air.local>
2023-01-28 16:36:46 +0800

    feat: add router transition

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
   <AppHeader />
-  <RouterView mt-12 />
+  <router-view v-slot="{ Component }" mt-12>
+    <Transition>
+      <component :is="Component" />
+    </Transition>
+  </router-view>
+  <!-- <RouterView mt-12 /> -->
   <AppFooter />
   <AppMask />
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.v-enter-active {
+  transition: opacity 0.3s ease-in;
+}
+.v-enter-from {
+  opacity: 0;
+}
+</style>
